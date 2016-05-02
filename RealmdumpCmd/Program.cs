1c1f090a815ec3821f910f7783789491bdae01c4
BlackRayquaza <shadowrealmrotmg@gmail.com>
2016-05-02 18:37:10 +0200

    hehe keeping console opened on start

--- RealmdumpCmd/Program.cs
+++ RealmdumpCmd/Program.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RealmdumpCmd
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        // ReSharper disable once UnusedParameter.Local
+        private static void Main(string[] args)
         {
+            Console.ReadLine();
         }
     }
 }
